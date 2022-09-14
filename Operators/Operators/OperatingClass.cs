b0be846ea35aee7e014671bec4389c51a1b0e58d
Kael Renaud <Kmans2272@gmail.com>
2022-09-14 12:00:03 -0500

    hey

---
+++ Operators/Operators/OperatingClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class OperatingClass
+    {
+        public static double Area(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Operators;
+
+Console.WriteLine("Enter in a number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+Console.WriteLine("Enter in another number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int divideMethod = OperatingClass.Divide(firstNum, secondNum);
+int modulusMethod = OperatingClass.Modulus(firstNum, secondNum);
+
+
+Console.WriteLine($"The sum of the numbers are: {OperatingClass.Add(firstNum, secondNum)}");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+
+Console.WriteLine($"The first number minus the second number is: {OperatingClass.Subtract(firstNum, secondNum)}");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+
+Console.WriteLine($"The first number times the second number is: {OperatingClass.Multiply(firstNum, secondNum)}");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+
+Console.WriteLine($"The first number divided by the second number is: {divideMethod} with a remainder of {modulusMethod}");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Do you want to find the area of a circle with me? \n" +
+                  "yes to continue|anything else to end the program");
+string answer = Console.ReadLine();
+if (answer == "yes")
+{
+    Console.WriteLine("Enter the radius of your circle:");
+    double radius = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"The area of the circle is... {OperatingClass.Area(radius)}");
+}
+else
+{
+    Console.WriteLine("I guess you dont get to do something cool ;(");
+}
