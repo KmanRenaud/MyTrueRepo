b5ffc62b439d5ae8ff07de9bcff041f25d03ddff
Kael Renaud <Kmans2272@gmail.com>
2022-09-19 11:13:57 -0500

    hi

---
+++ OverloadDaMethod/OverloadDaMethod/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadDaMethod
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(decimal num1, decimal num2, bool isDollar)
+        {
+            if (num1 + num2 < 1 && num1 + num2 > -1 && num1 + num2 != 0)
+            {
+                decimal n = num1 + num2;
+                return $"{n} cents";
+            }
+            if (num1 + num2 == 1 || num1 + num2 == -1)
+            {
+                isDollar = true;
+            }
+            else
+            {
+                isDollar = false;
+            }
+            if (isDollar == true)
+            {
+                return $"{num1 + num2} dollar";
+            }
+            else
+            {
+                return $"{num1 + num2} dollars";
+            }
+        }
+    }
+}
---
+++ OverloadDaMethod/OverloadDaMethod/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using OverloadDaMethod;
+
+Console.WriteLine(Methods.Add(2, 2));
+Console.WriteLine(Methods.Add(2.22m, -7.22m));
+Console.WriteLine(Methods.Add(.27m, -.5m, true));
+
