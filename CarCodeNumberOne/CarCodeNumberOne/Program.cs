9e9f42825feba6ef0ca88c9044711b866126cc60
Kael Renaud <Kmans2272@gmail.com>
2022-09-22 10:59:26 -0500

    hello CLASSES ASSIGNMENT

---
+++ CarCodeNumberOne/CarCodeNumberOne/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using CarCodeNumberOne;
+
+Cars Car1 = new Cars();
+Car1.Make = "Nissan";
+Car1.Model = "Altima";
+Car1.Year = 2012;
+
+Console.WriteLine($"The make of the car is {Car1.Make}, the model is {Car1.Model}, and the year it was made in is {Car1.Year}.");
