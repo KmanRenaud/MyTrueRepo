6908956538a132ca72603c3956162fe27483fdb5
Kael Renaud <Kmans2272@gmail.com>
2022-09-30 14:57:22 -0500

    Finished LinQ

---
+++ LINQProj/LINQProj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+//This one is FUN!
+
+List<string> gameNames = new List<string>() 
+{
+    "Destiny 2",
+    "Dark Souls",
+    "Call of Duty",
+    "Minecraft",
+    "Rocket League",
+    "God of War",
+    "Fortnite",
+    "Apex",
+    "Valorant",
+    "League of Legends"
+};
+
+int i = 1;
+
+gameNames.OrderBy(x => x.Length)
+         .ToList()
+         .ForEach(x => Console.WriteLine($"Game number {i++}: {x}"));
+
+
+#region
+
+//LINQ is the BOMBBBBB
+
+#endregion
