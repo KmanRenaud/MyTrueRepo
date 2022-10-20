c244676ab382b88e3e37e6856d432dfc43304582
Kael Renaud <85758273+KmanRenaud@users.noreply.github.com>
2022-10-20 09:31:10 -0500

    Delete BestBuyTrueCoders directory

--- BestBuyTrueCoders/BestBuyTrueCoders/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BestBuyTrueCoders.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
